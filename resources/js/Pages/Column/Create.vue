84d26b14e4de37be8c24ea121b2bcf24ed3dc9f7
MartinAdzhiev <adzhievmartin@gmail.com>
2024-03-05 16:05:57 +0100

    Created ColumnController, defined routes, implemented create, update and delete of columns with simple views

---
+++ resources/js/Pages/Column/Create.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {reactive, toRefs} from 'vue'
+import { router } from '@inertiajs/vue3'
+
+
+const props = defineProps({
+    board: Object
+})
+
+const { board } = toRefs(props);
+
+
+const form = reactive({
+    board_id: board.value.id,
+    name: null,
+})
+
+function submit() {
+    router.post('/column', form)
+}
+</script>
+
+<template>
+    <form @submit.prevent="submit">
+        <label for="board">{{board.name}}:</label>
+        <input disabled id="name" v-model="board.id"/>
+        <label for="name">Column Name:</label>
+        <input id="name" v-model="form.name" />
+        <button type="submit">Submit</button>
+    </form>
+</template>
